542da0d333a69bf104503dd7b5fdb06f9a2dba2a
Bhavin Jagad <jagad89@gmail.com>
2018-04-24 09:22:02 +0530

    setup start script for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 
 var app  = express();
 
@@ -53,6 +54,6 @@ app.get('/bad',(request,response)=>{
     });
 });
 
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
     console.log("server is running");
 });
